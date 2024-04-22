4ff1a56ed7c015f1f0be3ef528d7925611f56a0d
nizamuddin0172 <neom01723441081@gmail.com>
2024-04-22 11:22:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <person></person>
+    </>
+  );
+}
+
+function person () {
+
+}
+
+
+
+export default App;
